4be4aa2fab14d08fc4083f95dbc3b3c91dbfe0eb
Pavel Kutáč <pavel.kutac@gmail.com>
2019-08-19 12:15:51 +0200

    Offline pull request merge - output into template

--- src/convert.ts
+++ src/convert.ts
@@ -6,34 +6,48 @@ import { Config } from './Config/Config';
 import path from 'path';
 import { ContextRandomNumber } from './helpers/ContextRandomNumber';
 
-try {
-  const configPath = path.resolve(process.argv[2] || 'config.yaml');
+(async () => {
+  try {
+    const configPath = path.resolve(process.argv[2] || 'config.yaml');
 
-  if (!fs.pathExistsSync(configPath)) {
-    throw 'Pass path to config file or create config.yaml';
-  }
-
-  const config = Config.fromPlainObject(yaml.safeLoad(fs.readFileSync(configPath, 'utf8')));
-
-  ContextRandomNumber.init(config.random.seed || null, config.random.contextualSeed || false);
-
-  const generator = new RegexToSentenceGenerator(config);
-  generator.processEntities(config.entities);
-  const dfSentences = generator.processSentences(config.sentences);
-
-  if (config.output.stdout === true) {
-    console.log(dfSentences.toString());
-    console.info('\n');
-  }
-
-  if (typeof config.output.dialogFlowJSONFile === 'string') {
-    let outputPath = config.output.dialogFlowJSONFile;
-    if (!path.isAbsolute(outputPath)) {
-      outputPath = `${path.dirname(configPath)}${path.sep}${outputPath}`;
+    if (!fs.pathExistsSync(configPath)) {
+      throw 'Pass path to config file or create config.yaml';
     }
-    fs.writeFileSync(outputPath, JSON.stringify(dfSentences.toDialogFlowJSON(), null, 2));
+
+    const templatePath = process.argv[2] && process.argv[3];
+    const config = Config.fromPlainObject(yaml.safeLoad(await fs.readFile(configPath, 'utf8')));
+
+    ContextRandomNumber.init(config.random.seed || null, config.random.contextualSeed || false);
+
+    const generator = new RegexToSentenceGenerator(config);
+    generator.processEntities(config.entities);
+    const dfSentences = generator.processSentences(config.sentences);
+
+    if (config.output.stdout === true) {
+      console.log(dfSentences.toString());
+      console.info('\n');
+    }
+
+    if (typeof config.output.dialogFlowJSONFile === 'string') {
+      let outputPath = config.output.dialogFlowJSONFile;
+
+      if (!path.isAbsolute(outputPath)) {
+        outputPath = `${path.dirname(configPath)}${path.sep}${outputPath}`;
+      }
+
+      let template: any = {};
+      if (templatePath) {
+        template = await fs.readJSON(templatePath);
+        template.userSays = dfSentences.toDialogFlowJSON();
+      } else {
+        template = dfSentences.toDialogFlowJSON();
+      }
+
+      await fs.writeFile(outputPath, JSON.stringify(template, null, 2));
+    }
+
+    console.info(`Everything done - totally generated ${dfSentences.getAll().length} sentences`);
+  } catch (e) {
+    console.error(e);
   }
-  console.info(`Everything done - totally generated ${dfSentences.getAll().length} sentences`);
-} catch (error) {
-  console.error(error);
-}
+})();
